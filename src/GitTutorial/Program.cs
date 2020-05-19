06007f57a2bcb1422a54c74fb2b4dedfe966a6e3
odoyi09 <odoyi09@gmail.com>
2020-05-19 17:27:26 +0100

    added console project files

---
+++ src/GitTutorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
